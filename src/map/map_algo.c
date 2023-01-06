b990d21d016a562958cd197edcf0f57a6d463714
LolinEagle <francois-lolin@live.fr>
2023-01-06 15:00:08 +0100

    parsing_done

--- src/map/map_algo.c
+++ src/map/map_algo.c
@@ -12,10 +12,9 @@
 
 #include <cub3D.h>
 
-void	map_algo(t_cub3d *cub3d)
+void	map_spawn(t_cub3d *cub3d)
 {
 	size_t	i;
-	size_t	x;
 	size_t	y;
 
 	i = 0;
@@ -33,9 +32,34 @@ void	map_algo(t_cub3d *cub3d)
 			break ;
 		i++;
 	}
-	x = i;
-	if (!x || !y || x == cub3d->map_width - 1 || y == cub3d->map_height - 1)
-		free_exit(cub3d, "The map must be closed.\n");
-	cub3d->x = x + 0.5;
+	cub3d->x = i + 0.5;
 	cub3d->y = y + 0.5;
+}
+
+void	map_algo(t_cub3d *cub3d)
+{
+	size_t	x;
+	size_t	y;
+
+	y = -1;
+	while (cub3d->map[++y])
+	{
+		x = -1;
+		while (cub3d->map[y][++x])
+		{
+			if (char_in_string(cub3d->map[y][x], "0NSWE"))
+			{
+				if (x == 0 || y == 0
+					|| x == cub3d->map_width - 1 || y == cub3d->map_height - 1
+					|| char_in_string(cub3d->map[y][x - 1], "01NSWE") == false
+					|| char_in_string(cub3d->map[y][x + 1], "01NSWE") == false
+					|| x >= strlen_endl(cub3d->map[y - 1])
+					|| char_in_string(cub3d->map[y - 1][x], "01NSWE") == false
+					|| x >= strlen_endl(cub3d->map[y + 1])
+					|| char_in_string(cub3d->map[y + 1][x], "01NSWE") == false)
+					free_exit(cub3d, "Map not closed.\n");
+			}
+		}
+	}
+	map_spawn(cub3d);
 }
