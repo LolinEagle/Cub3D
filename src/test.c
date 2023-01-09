7db82ec430de1af9e41f6618ef8d895cd45c077a
Samantha Le Huec <s.lhuec@gmail.com>
2023-01-09 15:40:59 +0100

    test

---
+++ src/test.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 15:36:40 by sle-huec          #+#    #+#             */
+/*   Updated: 2023/01/09 15:38:31 by sle-huec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// une focntion pour lancer les rayons depuis la pos player
+
+
+// une fonction pour chercher quand le mur est touché
+
+//
