833894f578ef6dc60fe7ba5e2d9a016054d79ee2
Samantha Le Huec <s.lhuec@gmail.com>
2023-01-12 20:03:58 +0100

    add comments

--- src/wall_renderer/init_renderer.c
+++ src/wall_renderer/init_renderer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_renderer.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 15:36:40 by sle-huec          #+#    #+#             */
-/*   Updated: 2023/01/12 14:55:25 by sle-huec         ###   ########.fr       */
+/*   Updated: 2023/01/12 20:01:15 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,22 @@ void	init_var_renderer(t_cub3d *wall_renderer)
 	wall_renderer->v_camera_plane_x = 0;	
 }
 
-// NE PAS OUBLIER D ARRONDR AVE FLOOR
+// NE PAS OUBLIER D ARRONDIR AVEC FLOOR
 void	init_orientation_from_player(t_cub3d *wall_renderer)
 {
 	if()
 }
 
-// récupérer N / S / E/ W pour obtenir la pos du joueur
-// sauvegarder dir et pos et remplacer par 0;
-// générer le vecteur direction en fonction du symbole N / S / E / W
-// générer le vecteur plan de caméra
-// envoyer des rayons de guche à droite (un par pixel en fonction de la résolution)
-// depuis la pos du joueur
-// calculer la distance jouer / mur en charchat si le rayon a touché un mur
+// tester en statique:
+// récupérer N / S / E/ W avec les coordonnées x et y pour l'orientation de départ
+// sauvegarder dir et pos et remplacer la lettre par 0 (point de départ)
+// envoyer des rayons de gauche à droite (un par pixel en fonction de la résolution) depuis la pos du joueur
+// générer le vecteur plan de caméra à partir de distance
+// générer dir 
+// en déduire la distance
 // établir le ratio entre la distance et la taille de la colonne de pixel
 // définir les variables permettant de dessiner le mur (ligne haute, point de départ, position de fin)
+
+
+// gestion du mouvement ds les  directions avant/arrière, droite/gauche et de la rotation:
+// générer le vecteur direction en fonction des keycodes
