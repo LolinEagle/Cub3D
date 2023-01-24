eacda92ca1e4970bdcbd8c69407e4842de0fb52e
François <francois-lolin@live.fr>
2023-01-24 18:02:41 +0100

    texture_wip

--- src/wall_renderer/draw.c
+++ src/wall_renderer/draw.c
@@ -26,35 +26,45 @@ void	init_draw(t_cub3d *s)
 		s->draw_end = HEIGHT - 1;
 }
 
-void	draw_ver_line(t_cub3d *s, t_img *dst, t_img src)
+void	draw_ver_line(t_cub3d *s, t_img *dst, t_img src, double wall)
 {
 	int		h;
+	double	wall_size;
 
+	wall_size = (s->draw_end - s->draw_start) * (1.0 / 63.0);
 	h = s->draw_start;
 	while (h < s->draw_end)
 	{
 		dst->img_str[(s->col_x_iterator * 4) + (WIDTH * 4 * h) + 0] = \
-		src.img_str[(0 * 4) + (TEXTURE_SIZE * 4 * 0) + 0];
+		src.img_str[((int)round(wall * 64) * 4) + (TEXTURE_SIZE * 4 * \
+		(int)round((h - s->draw_start) / wall_size)) + 0];
 		dst->img_str[(s->col_x_iterator * 4) + (WIDTH * 4 * h) + 1] = \
-		src.img_str[(0 * 4) + (TEXTURE_SIZE * 4 * 0) + 1];
+		src.img_str[((int)round(wall * 64) * 4) + (TEXTURE_SIZE * 4 * \
+		(int)round((h - s->draw_start) / wall_size)) + 1];
 		dst->img_str[(s->col_x_iterator * 4) + (WIDTH * 4 * h) + 2] = \
-		src.img_str[(0 * 4) + (TEXTURE_SIZE * 4 * 0) + 2];
+		src.img_str[((int)round(wall * 64) * 4) + (TEXTURE_SIZE * 4 * \
+		(int)round((h - s->draw_start) / wall_size)) + 2];
 		h++;
 	}
-	(void)src;
 }
 
+// s->side = 0 (W, E)
+// s->side = 1 (N, S)
 void	draw(t_cub3d *s, t_img *img)
 {
 	t_img	src;
+	double	wall_hit_point;
 
-	// s->side = 0 (W, E)
-	// s->side = 1 (N, S)
 	if (s->side == 0)
 		src.img_str = mlx_get_data_addr(s->west, \
 			&src.bits, &src.line, &src.endian);
-	else// if (s->side == 1)
-		src.img_str = mlx_get_data_addr(s->north, \
+	else
+		src.img_str = mlx_get_data_addr(s->south, \
 			&src.bits, &src.line, &src.endian);
-	draw_ver_line(s, img, src);
+	if (s->side == 0)
+		wall_hit_point = s->y + s->perp_wall_dist * s->ray_dir_y;
+	else
+		wall_hit_point = s->x + s->perp_wall_dist * s->ray_dir_x;
+	wall_hit_point -= floor((wall_hit_point));
+	draw_ver_line(s, img, src, wall_hit_point);
 }
